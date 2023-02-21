c1bace2567e023e02513787ef0567d701c58b06d
TheTechAmazon <thetechamazon@gmail.com>
2023-02-21 15:56:24 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,11 +13,13 @@ int print_sign(int n)
 	{
 		putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n == 0)
 	{
 		putchar(48);
 		return (0);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
