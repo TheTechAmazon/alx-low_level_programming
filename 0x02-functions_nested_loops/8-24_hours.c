e71c9a936fe766849fbd3199b898245c04834031
TheTechAmazon <thetechamazon@gmail.com>
2023-02-21 17:33:53 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
