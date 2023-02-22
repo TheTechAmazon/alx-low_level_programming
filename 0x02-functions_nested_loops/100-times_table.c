938766b05191599eb1fbc58d62f6ba9c36fbe79a
TheTechAmazon <thetechamazon@gmail.com>
2023-02-21 20:15:04 -0800

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,8 @@
 void print_times_table(int n)
 {
 	int i, j;
-	if (n < 0 || n > 100)
+
+	if (n < 0 || n > 15)
 	{
 		return;
 	}
@@ -20,7 +21,7 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			printf("%4d", i * j);
+			printf("%d\t", i * j);
 		}
 		printf("\n");
 	}
