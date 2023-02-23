ff13460346e29ba66cdac8982b64e1b19a1065bf
TheTechAmazon <thetechamazon@gmail.com>
2023-02-22 19:11:50 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
+
 int main(void)
 {
 	int r;
-
 	print_last_digit(98);
 	print_last_digit(0);
 	r = print_last_digit(-1024);
