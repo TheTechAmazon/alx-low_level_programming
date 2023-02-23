ff9da2a6710cdce60b189f0872c522e209a39883
TheTechAmazon <thetechamazon@gmail.com>
2023-02-22 19:03:44 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,47 +1,28 @@
-#include <stdio.h>
-#include <limits.h>
-/**
-* Write a function that prints the sign of a number
-* print_sign - prints the sign of a number.
-* @n: the number to check.
-* Return: 1 if n is positive, -1 if n is negative, 0 if n is zero.
-*/
+#include "main.h"
 
+
+/**
+* print_sign - prints the sign of a number
+* @n: the int to check
+* Return: 1 and prints + if n is greater than zero
+* 0 and prints 0 if n is zero
+* -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar(48);
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
-		putchar ('-');
-		return (-1);
+		_putchar('-');
 	}
-}
-
-
-/**
-* This program includes functions to determine the sign of a number.
-* This file includes a helper function for determining the sign of a number.
-* This function takes an integer input and calls the print_sign function to print its sign to the console. 
-* It returns nothing
-*/
-
-void print_sign_function()
-{
-	print_sign(98);       /* Output: + */
-	print_sign(0);        /* Output: 0 */
-	print_sign(0xff);     /* Output: + */
-	print_sign(-1);       /* Output: - */
-	print_sign(99);       /* Output: + */
-	print_sign(INT_MAX);  /* Output: + */
-	print_sign(INT_MIN);  /* Output: - */
-	print_sign(-4);       /* Output: - */
+	return (-1);
 }
