86432d2e229e598d4bbc5ef960f69706efd1ef26
TheTechAmazon <thetechamazon@gmail.com>
2023-04-14 19:45:19 -0700

    correct textfile task 0 by techamazon

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,34 +7,25 @@
  * @filename: this is the text filename being read
  * @letters: the numbers of letters to be read and printed
  *
- * Return: the actual numbers of letetrs read and printed
+ * Return: the actual numbers of letters read and printed
  * else return 0 when functions fails
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letetrs);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buffer = malloc(sizeof(cahr) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
+	free(buf);
+	close(fd);
 	return (w);
 }
