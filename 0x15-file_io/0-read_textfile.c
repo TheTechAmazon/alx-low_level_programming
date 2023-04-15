64655b6f1238576b7e270694d4d597b75c3843ba
TheTechAmazon <thetechamazon@gmail.com>
2023-04-14 19:14:18 -0700

    corrected textfile task 0 by techamazon

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(cahr) * (letters));
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
