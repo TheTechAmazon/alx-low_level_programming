3d73b1cafadb30d1f3f1b2ec5c24ebfb2f47bee4
TheTechAmazon <thetechamazon@gmail.com>
2023-04-21 16:04:04 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,9 +3,8 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntk.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
