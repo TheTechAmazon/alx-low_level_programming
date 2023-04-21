f37c6c5649a32d0da0a2dfa6ad7bacfcbc1a9d85
TheTechAmazon <thetechamazon@gmail.com>
2023-04-21 15:54:48 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 /**
  * create_file - This create a file
- * @filename; This is the filename to be created
+ * @filename: This is the filename to be created
  * @text_content: The content written in the file
  *
  * Return: -1 if the function fails otherwise 1 if the function succeed.
@@ -18,13 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for ( len = 0, text_content[len];)
+		for (len = 0, text_content[len];)
 			len++;
 	}
 
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	
 	rwr = write(fd, text_content, nletters);
 
 	if (fd == -1 || rwr == -1)
