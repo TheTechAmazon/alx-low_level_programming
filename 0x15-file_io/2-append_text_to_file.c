bb25dcaaa39c583b9ca586901f461ce5ad43eb5f
TheTechAmazon <thetechamazon@gmail.com>
2023-04-27 09:44:23 -0700

    corrected and updated files and main.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,11 +3,10 @@
 /**
  * append_text_to_file - This appends the text to the end of the file.
  * @filename: This is the pointer to the name of the file
- * @textcontent: the string to add to the content of the file
+ * @text_content: the string to add to the content of the file
  *
- * return: if the file exist 1
- * if the file does not exist -1
- * if the function fails or filename is NULL -1
+ * Return: 1 if the file exist, -1 if the file does not exist
+ * or -1 if the function fails or filename is NULL.
  */
 
 int append_text_to_file(const char *filename, char *text_content)
