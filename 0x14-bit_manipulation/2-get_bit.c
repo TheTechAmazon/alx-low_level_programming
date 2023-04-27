6ee0077784597eb65fb482a9f2c0dd02e78c5900
TheTechAmazon <thetechamazon@gmail.com>
2023-04-26 21:19:47 -0700

    the value of a bit in a file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * get_bit - To get the vlue of a bit at a given index to unsigned int
- * @n: The number to evaluate char string
+ * get_bit - To get the value of a bit at a given index
+ * in a decimal number to unsigned int
+ * @n: The number to evaluate or search in char string
  * @index: Index starting from 0, of the bit.
  * Return: The value of bit at index, or -1 if no success
  */
@@ -11,10 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int hold;
 
-	if (i > 64)
+	if (index > 64)
 		return (-1);
 
-	hold = n >> i;
+	hold = n >> index;
 
 	return (hold & 1);
 }
