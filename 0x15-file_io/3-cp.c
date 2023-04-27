194bf3ca51cf7ea7975eb2962697c9049720c928
TheTechAmazon <thetechamazon@gmail.com>
2023-04-27 10:58:21 -0700

    corrected and updated file that copies the content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - 1024 bytes allocated for a buffer.
+ * @file: The name of the file the buffer is storing chars for.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: Pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
@@ -28,7 +28,7 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - Closes file descriptors.
+ * close_file - This close the file descriptors.
  * *@fd: The file descriptors to be closed.
  */
 void close_file(int fd)
@@ -40,20 +40,21 @@ void close_file(int fd)
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * #argv: An array of pointers to the arguments.
+ * main - This copies the contents of one file to another.
+ * @argc: The number of arguments count supplied to the program.
+ * @argv: An array of pointers to the arguments passed.
  *
  * Return: 0 on success.
  *
- * Description; If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannnot be closed - exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannnot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
