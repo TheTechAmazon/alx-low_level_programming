83cceb74101516c7c752f7f895e0e47825b13fef
TheTechAmazon <thetechamazon@gmail.com>
2023-04-28 04:35:38 -0700

    updated files that copies the content of a file to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define WRITE_ERR "Error: Can't write to %s\n"
+#define READ_ERR "Error: Can't read from %s\n"
 
 /**
  * main - This copy the contents of one file to the other.
@@ -13,25 +15,28 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, WRITE_ERR, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[1]), exit(99);
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file_to == -1)
-		dprintf(STDERR_FILENO, READ_ERR, argv[2]), exit(99);
-	nchars = 1024;
-	while (nchars == 1024)
+		dprintf(STDERR_FILENO, READ_ERR, argv[2]), exit(98);
+	while (1)
 	{
 		nchars = read(file_from, buffer, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buffer, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
+			dprintf(STDERR_FILENO, READ_ERR, argv[2]), exit(98);
+		if (nchars > 0)
+			nwr = write(file_to, buffer, nchars);
+		else if (nwr -- -1)
+			dprintf(STDERR_FILENO, WRITE_ERR, argv[1]), exit(99);
+		else
+			break;
 	}
 	err_close = close(file_from);
 	if (err_close == -1)
@@ -40,5 +45,6 @@ int main(int argc, char *argv[])
 	if (err_close == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
-	return (0);
+	return 0;
+
 }
