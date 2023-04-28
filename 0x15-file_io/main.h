e9b06bb0b044d2df1b9d55444a8d5f2197d1e9ff
TheTechAmazon <thetechamazon@gmail.com>
2023-04-27 17:15:27 -0700

    updated main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include < fcntl.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -15,13 +15,13 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char e_ident);
-void print_class(unsigned char e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char*e_ident);
 void print_osabi(unsigned char *e_ident);
-void print_types(unsigned int e_type, unsigned char e_ident);
+void print_types(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf)
 #endif
